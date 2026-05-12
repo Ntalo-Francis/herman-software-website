875b1bf8c7f30ae7e2642bdd588fb91c9b474538
Jiang Salim <jaingsalim@gmail.com>
2026-05-11 23:27:25 -0700

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -48,8 +48,8 @@ export async function sendEmail({ subject, html }: SendEmailParams) {
     const transport = await getTransporter();
 
     const mailOptions = {
-      from: "infohermansoftware@gmail.com",
-      to: process.env.EMAIL_TO || "infohermansoftware@gmail.com",
+      from: "jaingsalim@gmail.com",
+      to: process.env.EMAIL_TO || "jaingsalim@gmail.com",
       subject,
       html,
     };
